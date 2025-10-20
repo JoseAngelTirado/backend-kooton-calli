c0bd9b4b666aeb69055134cf69221d48640419ea
fragosonic <fragosonic98@gmail.com>
2025-10-19 18:01:54 -0600

    Add: SalesTicketService.java

---
+++ src/main/java/com/kootoncalli/kooton_calli/service/SalesTicketService.java
@@ -0,0 +1,14 @@
+package com.kootoncalli.kooton_calli.service;
+
+import com.kootoncalli.kooton_calli.dto.SalesTicketDto;
+
+public interface  SalesTicketService {
+      // Crud Methods
+      SalesTicketDto save(SalesTicketDto salesTicketDto);
+      SalesTicketDto findById(Integer id);
+
+      Iterable<SalesTicketDto> findAll();
+      SalesTicketDto update(Integer idSaleTicket, SalesTicketDto salesTicketDto);
+
+      public SalesTicketDto getSaleTicketById(Integer id);
+}
